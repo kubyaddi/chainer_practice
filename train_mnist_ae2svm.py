227d64bc0d31c9f7e8cf5a3f63d9047558c8218f
kubyaddi <keiu@gmail.com>
2015-07-01 18:59:25 +0900

    improved

--- train_mnist_ae2svm.py
+++ train_mnist_ae2svm.py
@@ -1,18 +1,32 @@
 #/usr/bin/env python
 
+import numpy as np
 from chainer import Variable, Function, FunctionSet, optimizers
 import chainer.functions as F
+from sklearn.datasets import fetch_mldata
+from sklearn import svm
 
 from auto_encoder import AutoEncoder
 
-from sklearn.datasets import fetch_mldata
-import numpy as np
-
 batchsize = 100
-n_epoch = 4
+n_epoch = 5
 n_units = 100
 
-mnist = fetch_mldata('MNIST original')
+def fetch_mnist():
+    import os.path
+    if(os.path.exists("MNISTorg.pklz")):
+        import gzip
+        try:
+            import cPickle as pickle
+        except:
+            import pickle
+        mf = gzip.open("MNISTorg.pklz")
+        dat = pickle.load(mf)
+        return dat
+    else:
+        return fetch_mldata("MNIST original")
+
+mnist = fetch_mnist()
 mnist.data = mnist.data.astype(np.float32)
 mnist.data /= 255
 mnist.target = mnist.target.astype(np.int32)
@@ -42,15 +56,15 @@ for epoch in xrange(1, n_epoch+1):
     print 'train mean loss={}'.format(sum_loss/N)
 
 #1layer SVM
-from sklearn import svm
-clf = svm.LinearSVC()
-f_train = ae1.encode(Variable(x_train), train=False).data
-clf.fit(f_train, y_train)
-f_test = ae1.encode(Variable(x_test), train=False).data
-print "1layer test mean score={}".format(clf.score(f_test, y_test))
+f_train1 = ae1.encode(Variable(x_train), train=False).data
+#clf1 = svm.SVC()
+clf1 = svm.LinearSVC()
+clf1.fit(f_train1, y_train)
+f_test1 = ae1.encode(Variable(x_test), train=False).data
+print "1layer test mean score={}".format(clf1.score(f_test1, y_test))
 
 #Second layer
-ae2 = AutoEncoder(n_units, n_units/2)
+ae2 = AutoEncoder(n_units, n_units)
 
 optimizer2 = optimizers.Adam()
 optimizer2.setup(ae2.model.collect_parameters())
@@ -70,10 +84,10 @@ for epoch in xrange(1, n_epoch+1):
     print 'train mean loss={}'.format(sum_loss/N)
 
 #2layer SVM
-from sklearn import svm
-clf = svm.LinearSVC()
-f_train = ae2.encode(ae1.encode(Variable(x_train), train=False), train=False).data
-clf.fit(f_train, y_train)
-f_test = ae2.encode(ae1.encode(Variable(x_test), train=False), train=False).data
-print "2layer test mean score={}".format(clf.score(f_test, y_test))
+f_train2 = ae2.encode(Variable(f_train1), train=False).data
+#clf2 = svm.SVC()
+clf2 = svm.LinearSVC()
+clf2.fit(f_train2, y_train)
+f_test2 = ae2.encode(Variable(f_test1), train=False).data
+print "2layer test mean score={}".format(clf2.score(f_test2, y_test))
 
